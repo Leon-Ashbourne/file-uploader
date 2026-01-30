0ebbea002f7bdb631be5cbb999c80a62e16d8a7d
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-01-30 23:01:20 +0530

    add a new route for folders

--- controllers/libController.js
+++ controllers/libController.js
@@ -1,11 +1,17 @@
 const getFilesFromDB = require("../models/script").getFilesFromDB;
+const getFoldersFromDB = require("../models/script").getFoldersFromDB;
 
 //files list
 async function requestAllFiles(req, res, next) {
     try{
-        const files = await getFilesFromDB(req.user.id);
-        res.locals.files = files;
-        next();
+        const files = getFilesFromDB(req.user.id);
+        const folders = getFoldersFromDB(req.user.id);
+        Promise.all([files, folders])
+            .then(([val1, val2]) => {
+                res.locals.files = val1;
+                res.locals.folders = val2;
+                next();
+            })
     }catch(err) {
         next(err);
     }
@@ -13,10 +19,17 @@ async function requestAllFiles(req, res, next) {
 
 async function updateFileDetails(req, res, next) {
     const path = req.originalUrl;
-    const files = res.locals.files
+
+    const files = res.locals.files;
     res.locals.files = files.map(file => {
         file.url = path + "/files/" + file.OriginalName;
         return file;
+    });
+
+    const folders = res.locals.folders;
+    res.locals.folders = folders.map(folder => {
+        folder.url = path + "/d0f4e1548ad9e4f162300/" + folder.id;
+        return folder;
     })
     res.locals.user = req.user;
     next();
--- routes/libRouter.js
+++ routes/libRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
+
 const libController = require("../controllers/libController");
 const filesController = require("../controllers/filesController");
 const authLibraryRouter = require("./auth/authLibraryRouter");
+const folderController = require("../controllers/folderRouter");
 
 const libRouter = Router();
 
 libRouter.post("/files", filesController.filesPost);
+libRouter.post("/folder", folderController.folderGet);
 libRouter.get("/", authLibraryRouter, libController.libGet);
 
 module.exports = libRouter;
