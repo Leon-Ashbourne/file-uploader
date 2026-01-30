3797eb0979e61661eb39ac0cda6abf09e848801e
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-01-30 16:10:08 +0530

    fix error- add 'post' route on '/library' route

--- routes/libRouter.js
+++ routes/libRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const libController = require("../controllers/libController");
+const filesController = require("../controllers/filesController");
 const authLibraryRouter = require("./auth/authLibraryRouter");
 
 const libRouter = Router();
 
+libRouter.post("/files", filesController.filesPost);
 libRouter.get("/", authLibraryRouter, libController.libRender);
 
 module.exports = libRouter;
